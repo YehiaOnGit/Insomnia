ba0524552ec4c048c8db8a4ce4d44225da2e8279
Yehia Mohamed <93736300+YehiaOnGit@users.noreply.github.com>
2022-11-23 16:40:53 +0200

    Update discovernearbydevices.py
    added user detection & music

--- discovernearbydevices.py
+++ discovernearbydevices.py
@@ -1,7 +1,11 @@
 import bluetooth
-
+import playsound
 while True:
     nearby_devices = bluetooth.discover_devices(lookup_names=True)
     print("Found {} devices.".format(len(nearby_devices)))
-    for addr, name in nearby_devices:
-        print("  {} - {}".format(addr, name))
+    if len(nearby_devices) == 0:
+        playsound.playsound('C:/Users/yehia/Desktop/waiting.wmv')
+    elif len(nearby_devices) > 0:
+        playsound.playsound('C:/Users/yehia/Desktop/found.wmv')
+        for addr, name in nearby_devices:
+            print("  {} - {}".format(addr, name))
